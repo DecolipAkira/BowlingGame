ac7e1b9d47e7a4b63990ac6f45c74f8e9b20276a
André Luis <decolip2@gmail.com>
2019-06-08 19:04:08 -0300

    Commit Initial

---
+++ tests/BowlingGameTest.php
@@ -0,0 +1,60 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Kata\Game;
+
+class BowlingGameTest extends TestCase
+{
+    public function setUp() {
+        $this->game = new Game();
+    }
+    
+    public function testGutterGame() {
+        $this->rollMany(20, 0);
+
+        $this->assertEquals(0, $this->game->score());
+    }
+
+    public function rollMany($n, $pins) {
+        for($i = 0; $i < $n; $i++)
+            $this->game->roll($pins);
+    }
+
+    public function testAllOnes() {
+        $this->rollMany(20, 1);
+
+        $this->assertEquals(20, $this->game->score());
+    }
+
+    public function testeOneSpare() {
+        $this->rollSpare();
+        $this->game->roll(3);
+        $this->rollMany(17, 0);
+
+        $this->assertEquals(16, $this->game->score());
+    }
+
+    public function testOneStrike() {
+        $this->rollStrike();
+        $this->game->roll(3);
+        $this->game->roll(4);
+        $this->rollMany(16, 0);
+
+        $this->assertEquals(24, $this->game->score());
+    }
+
+    public function testPerfectGame() {
+        $this->rollMany(12, 10);
+        
+        $this->assertEquals(300, $this->game->score());
+    }
+
+    private function rollStrike() {
+        $this->game->roll(10);
+    }
+
+    private function rollSpare() {
+        $this->game->roll(5);
+        $this->game->roll(5);
+    }
+}
